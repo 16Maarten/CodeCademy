33b12e258a6227952f537ec8beedef46c365374e
16Maarten <176maarten@gmail.com>
2021-04-18 10:44:07 +0200

    Bij CourseWatch de mogelijkheid om content te bekijken toegevoegd

--- src/Presentation/CourseInfoUI/CourseWatchUI.java
+++ src/Presentation/CourseInfoUI/CourseWatchUI.java
@@ -17,18 +17,25 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import person.Registration;
 import products.Course;
+import products.Webcast;
+import products.Module;
 
 public class CourseWatchUI {
 
     private RegistrationManager registrationManager;
     private CourseManager courseManager;
     private List<Registration> registrations;
+    private List<Course> courses;
+    private List<Module> modules;
+    private List<Webcast> webcasts;
     private String email;
     private String course;
+    private String lesson;
 
     public CourseWatchUI(RegistrationManager registrationManager, CourseManager courseManager) {
         this.registrationManager = registrationManager;
         this.courseManager = courseManager;
+        courses = this.courseManager.getCourses();
     }
 
     public Parent getView() {
@@ -45,27 +52,37 @@ public class CourseWatchUI {
 
         Label name = new Label("Select Email");
         Label name2 = new Label("Select Course");
-        Label courseNameOutput = new Label();
-        Label subjectOutput = new Label();
-        Label introtextOutput = new Label();
-        Label difficultyIndicatorOutput = new Label();
+        Label name3 = new Label("Select Lesson");
+
         ComboBox studentsField = new ComboBox(FXCollections.observableArrayList(studentsEmails));
+        ComboBox contenItemsField = new ComboBox(FXCollections.observableArrayList(contentItems));
         ComboBox coursesField = new ComboBox(FXCollections.observableArrayList(coursesNames));
         coursesField.setMaxWidth(Double.MAX_VALUE);
-        ComboBox contenItemsField = new ComboBox(FXCollections.observableArrayList(contentItems));
         contenItemsField.setMaxWidth(Double.MAX_VALUE);
         Button submitButton = new Button("Submit");
         submitButton.setMaxWidth(Double.MAX_VALUE);
+        Button watchedButton = new Button("Watched");
+        watchedButton.setMaxWidth(Double.MAX_VALUE);
         Button submitButton2 = new Button("Submit");
         submitButton2.setMaxWidth(Double.MAX_VALUE);
+        Button submitButton3 = new Button("Submit");
+        submitButton3.setMaxWidth(Double.MAX_VALUE);
         studentsField.setMaxWidth(Double.MAX_VALUE);
         Button backButton = new Button("Back");
         CourseInfoUI courseInfoUI = new CourseInfoUI();
         backButton.setMaxWidth(Double.MAX_VALUE);
         Button infoButton = new Button("Info course");
         infoButton.setMaxWidth(Double.MAX_VALUE);
-        layout.getChildren().addAll(name, studentsField, submitButton, backButton);
 
+        Label contentTitle = new Label();
+        Label contentPublicationDate = new Label();
+        Label contentDescription = new Label();
+        Label contentInfo1 = new Label();
+        Label contentInfo2 = new Label();
+        Label contentInfo3 = new Label();
+        Label contentInfo4 = new Label();
+
+        layout.getChildren().addAll(name, studentsField, submitButton, backButton);
         layout.setSpacing(5);
 
         backButton.setOnAction((event) -> {
@@ -81,24 +98,72 @@ public class CourseWatchUI {
                         coursesNames.add(registrations.get(i).getCursusName());
                     }
                 }
+                coursesField.setItems(FXCollections.observableArrayList(coursesNames));
             }
+
             name.setText("Email: " + this.email);
             layout.getChildren().clear();
             layout.getChildren().addAll(name, name2, coursesField, submitButton2, backButton);
         });
-        
+
         submitButton2.setOnAction((event) -> {
             this.course = String.valueOf(coursesField.getValue());
-            for (int i = 0; i < registrations.size(); i++) {
-                if (registrations.get(i).getEmail().equals(this.email)) {
-                    if (!coursesNames.contains(registrations.get(i).getEmail())) {
-                        coursesNames.add(registrations.get(i).getCursusName());
+            for (int i = 0; i < courses.size(); i++) {
+                if (courses.get(i).getCursusName().equals(this.course)) {
+                    this.modules = courses.get(i).getModuleList();
+                    this.webcasts = courses.get(i).getWebcastList();
+                    for (int j = 0; j < this.modules.size(); j++) {
+                        if (!contentItems.contains(this.modules.get(j).getId() + " " + this.modules.get(j).getTitle())) {
+                            contentItems.add(this.modules.get(j).getId() + " " + this.modules.get(j).getTitle());
+                        }
+                    }
+                    for (int j = 0; j < this.webcasts.size(); j++) {
+                        if (!contentItems.contains(this.webcasts.get(j).getId() + " " + this.webcasts.get(j).getTitle())) {
+                            contentItems.add(this.webcasts.get(j).getId() + " " + this.webcasts.get(j).getTitle());
+                        }
                     }
                 }
             }
-            name2.setText("Course: " + this.email);
+            contenItemsField.setItems(FXCollections.observableArrayList(contentItems));
+            name2.setText("Course: " + this.course);
+            name3.setText("Select lesson: ");
             layout.getChildren().clear();
-            layout.getChildren().addAll(name, name2, coursesField, submitButton2, backButton);
+            layout.getChildren().addAll(name, name2, name3, contenItemsField, submitButton3, backButton);
+        });
+
+        submitButton3.setOnAction((event) -> {
+            this.lesson = String.valueOf(contenItemsField.getValue());
+            String[] parts = this.lesson.split(" ");
+            int contentId = Integer.valueOf(parts[0]);
+            submitButton2.setText("Choose an other lesson");
+            name3.setText("Lesson: " + this.lesson);
+            for (int i = 0; i < modules.size(); i++) {
+                if (this.modules.get(i).getId() == contentId) {
+                    contentTitle.setText("Title: " + this.modules.get(i).getTitle());
+                    contentPublicationDate.setText("Publication date: " + String.valueOf(this.modules.get(i).getPublicationDate()));
+                    contentDescription.setText("Description: " + this.modules.get(i).getDescription());
+                    contentInfo1.setText("Version: " + String.valueOf(this.modules.get(i).getVersion()));
+                    contentInfo2.setText("Contactperson: " + this.modules.get(i).getContactPersonName());
+                    contentInfo3.setText("Contactperson email: " + this.modules.get(i).getContactPersonEmail());
+                    layout.getChildren().clear();
+                    layout.getChildren().addAll(name, name2, name3, contentTitle, contentInfo1, contentPublicationDate, contentDescription, contentInfo2, contentInfo3, watchedButton, submitButton2, backButton);
+                }
+            }
+
+            for (int i = 0; i < webcasts.size(); i++) {
+                if (this.webcasts.get(i).getId() == contentId) {
+                    contentTitle.setText("Title: " + this.webcasts.get(i).getTitle());
+                    contentPublicationDate.setText("Publication date: " + String.valueOf(this.webcasts.get(i).getPublicationDate()));
+                    contentDescription.setText("Description: " + this.webcasts.get(i).getDescription());
+                    contentInfo1.setText("URL: " + String.valueOf(this.webcasts.get(i).getUrl()));
+                    contentInfo2.setText("Duration: " + this.webcasts.get(i).getDuration());
+                    contentInfo3.setText("Expert Name: " + this.webcasts.get(i).getExpertName());
+                    contentInfo4.setText("Organisation: " + this.webcasts.get(i).getExpertOrganisation());
+                    layout.getChildren().clear();
+                    layout.getChildren().addAll(name, name2, name3, contentTitle, contentPublicationDate, contentDescription, contentInfo1, contentInfo2, contentInfo3, contentInfo4, watchedButton, submitButton2, backButton);
+                }
+            }
+
         });
 
         infoButton.setOnAction((event) -> {
